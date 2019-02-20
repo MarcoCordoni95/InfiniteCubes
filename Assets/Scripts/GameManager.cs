fc40c7065387872d49d4671f65b5d5f5d8d36431
Marco Cordoni <marco.cordoni@outlook.it>
2019-02-20 22:43:45 +0100

    First commit

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour {
+
+    public float restartDelay = 1f;
+    public GameObject completeLevelUI;
+
+    private bool gameHasEnded = false;
+
+    public void EndGame() {
+
+        if (!gameHasEnded) {
+            gameHasEnded = true;
+            Invoke("Restart", restartDelay);
+        }
+    }
+
+    public void Restart() {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    public void CompleteLevel() {
+        completeLevelUI.SetActive(true);
+    }
+}
